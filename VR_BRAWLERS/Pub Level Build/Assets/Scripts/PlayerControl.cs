a3023a094db83981ba476d66d3589eb6108712bc
Hani Al Sibai <hanialsibai@gmail.com>
2019-02-21 17:48:11 +0000

    V0.2
    Music added, refined textures, cleaned folders

---
+++ VR_BRAWLERS/Pub Level Build/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	
+	
+	public float strafeSpeed = 150.0f;
+	public float runSpeed = 3.0f;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * strafeSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * runSpeed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
