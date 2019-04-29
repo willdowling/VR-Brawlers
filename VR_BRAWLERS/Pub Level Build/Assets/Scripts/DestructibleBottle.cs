47bc22ca23606ebc9e2af221b9054d6fd5d09844
williamd43 <willdowling77@gmail.com>
2019-04-29 11:19:50 +0100

    made hollow models for broken Bottles and better handling for VR

--- VR_BRAWLERS/Pub Level Build/Assets/Scripts/DestructibleBottle.cs
+++ VR_BRAWLERS/Pub Level Build/Assets/Scripts/DestructibleBottle.cs
@@ -1,35 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using VRTK;
 public class DestructibleBottle : MonoBehaviour
 {
-    [SerializeField] GameObject destroyedVersion;
-    [SerializeField] float explodeAmount = 5;
+    [SerializeField] GameObject broken_s;
+    [SerializeField] GameObject broken_l;
+    [SerializeField] GameObject broken_w;
+    [SerializeField] GameObject broken_weapon;
+    [SerializeField] float explodeAmount = 2;
 
     private Rigidbody rb;
+    private VRTK_InteractableObject bottle;
+    private bool bKnife = false;
 
-    private Vector3 pos, lastPos, lastVel;
+    private Vector3 pos, lastPos, lastVel, cpos;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        bottle = GetComponent<VRTK_InteractableObject>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        //Debug.Log(pos + ", " + lastPos);
-        if (collision.relativeVelocity.magnitude > 3.25f && collision.collider.gameObject.tag != "Controller")
+        if (collision.relativeVelocity.magnitude > 3.25f && collision.relativeVelocity.magnitude < 7f && !bottle.IsGrabbed())
         {
-            //Vector3 bottleVel = rb.velocity;
-            Debug.Log("Bottle Vel: " + lastVel);
-
-            Transform destroyed = Instantiate(destroyedVersion, transform.position, transform.rotation).transform;
+            Transform destroyed = Instantiate(broken_s, transform.position, transform.rotation).transform;
             foreach (Transform shard in destroyed) {
                 Vector3 centreToThis = (shard.position - destroyed.position).normalized;
                 shard.GetComponent<Rigidbody>().AddForce((centreToThis * explodeAmount) + lastVel, ForceMode.Impulse);
             }
 
+            Destroy(gameObject);
+        }else if (collision.relativeVelocity.magnitude > 7 && !bottle.IsGrabbed())
+        {
+            Transform destroyed = Instantiate(broken_l, transform.position, transform.rotation).transform;
+            foreach (Transform shard in destroyed) {
+                Vector3 centreToThis = (shard.position - destroyed.position).normalized;
+                shard.GetComponent<Rigidbody>().AddForce((centreToThis * explodeAmount) + lastVel, ForceMode.Impulse);
+            }
+
+            Destroy(gameObject);
+        }else if (collision.relativeVelocity.magnitude > 3f && bottle.IsGrabbed() && !bKnife)
+        {
+
+            
+            bKnife = true;
+            Transform destroyed = Instantiate(broken_w, transform.position, transform.rotation).transform;
+            foreach (Transform shard in destroyed) {
+                Vector3 centreToThis = (shard.position - destroyed.position).normalized;
+                shard.GetComponent<Rigidbody>().AddForce((centreToThis * explodeAmount) + lastVel, ForceMode.Impulse);
+            }
+            Transform t = Instantiate(broken_weapon, bottle.GetGrabbingObject().transform.position, bottle.GetGrabbingObject().transform.rotation).transform;
+            bottle.Ungrabbed();
             Destroy(gameObject);
         }
     }
@@ -43,6 +67,10 @@ public class DestructibleBottle : MonoBehaviour
     void Update()
     {
         pos = transform.position;
+        if (bKnife)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void LateUpdate()
